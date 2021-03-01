7f33a0f891d7f1887df9b142d6f73d3d50812e1a
rdubey <rdubey@teksystems.com>
2021-03-01 12:40:02 +0530

    Add project files.

---
+++ RetrieveDataFromFile/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RetrieveDataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Retrieve Student Data from a Text File***");
+
+        }
+    }
+}
