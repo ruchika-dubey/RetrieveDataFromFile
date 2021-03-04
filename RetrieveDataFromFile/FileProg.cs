8f07f209ebc1a74cbd948dfb726a706a07e09fb3
rdubey <rdubey@teksystems.com>
2021-03-04 11:43:02 +0530

    adding sort and search features

--- RetrieveDataFromFile/FileProg.cs
+++ RetrieveDataFromFile/FileProg.cs
@@ -9,17 +9,16 @@ namespace RetrieveDataFromFile
     {
         public static void Do()
         {
-            
+
             WriteLine();
-           
             ReadLine();
+            
 
 
-        
         }
-       
 
-        
+
+
 
         private static void WriteLine()
         {
@@ -35,17 +34,17 @@ namespace RetrieveDataFromFile
             }
 
             StreamWriter wrt = File.CreateText(filename);
-            wrt.WriteLine("Ruchika,21,ETC.");
-            wrt.WriteLine("Prateek,23,CS.");
-            wrt.WriteLine("Pragya,21,IT.");
-            wrt.WriteLine("Nidhi,23,ETC.");
-            wrt.WriteLine("Mayank,21,ETC.");
-            wrt.WriteLine("Pritika,20,CS.");
-            wrt.WriteLine("Kishwar,21,IT.");
+            wrt.WriteLine("Ruchika,ETC");
+            wrt.WriteLine("Prateek,CS");
+            wrt.WriteLine("Pragya,IT");
+            wrt.WriteLine("Nidhi,ETC");
+            wrt.WriteLine("Mayank,ETC");
+            wrt.WriteLine("Pritika,CS");
+            wrt.WriteLine("Kishwar,IT");
 
             wrt.Close();
             Console.WriteLine("Content lines have been added into file");
-            
+
         }
 
         private static void ReadLine()
@@ -56,63 +55,111 @@ namespace RetrieveDataFromFile
             {
                 Console.WriteLine("File exists");
                 string[] contents = File.ReadAllLines(filename);
-                
+                Console.WriteLine("*****Displaying contents in the file*****");
+                Print(contents);
+            }
 
-
-                    foreach (var content in contents)
+            else
+            {
+                Console.WriteLine("File doesn't exist");
+            }
+        }
+        public static void SortByName()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string filename = dir + "\\dataLine.txt";
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("File exists");
+                string[] contents = File.ReadAllLines(filename);
+                Console.WriteLine("*****Sorting contents in the file by name*****");
+                string temp;
+                foreach (var content in contents)
                 {
-                    Console.Write("Name:");
-                    int i;
-                    for (i = 0; content[i] != ','; i++)
-                    {
-                        if (content[i] != ',')
-                        {
-                            Console.Write(content[i]);
 
-                        }
-                        else
+                    for (int j = 0; j < contents.Length - 1; j++)
+                    {
+                        if (contents[j + 1].CompareTo(contents[j]) < 0)
                         {
-                            break;
+
+                            temp = contents[j];
+                            contents[j] = contents[j + 1];
+                            contents[j + 1] = temp;
+
+
                         }
                     }
-                    Console.Write(" Age:");
-                    for (i++; content[i] != ','; i++)
-                    {
-                        if (content[i] != ',')
-                        {
-                            Console.Write(content[i]);
 
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    Console.Write(" Dept:");
-                    for (i++; content[i] != '.'; i++)
-                    {
-                        if (content[i] != '.')
-                        {
-                            Console.Write(content[i]);
-
-                        }
-                        else
-                        {
-                            break;
-                        }
-
-                    }
-                    Console.Write("\n");
                 }
+                Print(contents);
             }
             else
             {
                 Console.WriteLine("File doesn't exist");
             }
         }
+        public static void SearchByName(string name)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string filename = dir + "\\dataLine.txt";
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("File exists");
+                string[] contents = File.ReadAllLines(filename);
+ 
+                int flag = 0;
+                foreach (var content in contents)
+                {
+
+                    if (content.Contains(name))
+                    {
+                        Console.WriteLine("*****data exists*****");
+                        Console.Write("Name:");
+                        int i;
+                        for (i = 0; content[i] != ','; i++)
+                            Console.Write(content[i]);
+                        Console.Write(" Class:");
+                        for (i++; i < content.Length; i++)
+                            Console.Write(content[i]);
 
 
-       
+                        Console.Write("\n");
+                        flag = 1;
+                        break;
+                    }
+                   
 
+
+                }
+                if(flag==0)
+                {
+                    
+                        Console.WriteLine("no data found corresponding to the name !");
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("File doesn't exist");
+            }
+
+        }
+        private static void Print(string[] contents)
+        {
+            foreach (var content in contents)
+            {
+                Console.Write("Name:");
+                int i;
+                for (i = 0; content[i] != ','; i++)
+                    Console.Write(content[i]);
+                Console.Write(" Class:");
+                for (i++; i < content.Length; i++)
+                    Console.Write(content[i]);
+
+
+                Console.Write("\n");
+            }
+
+        }
     }
 }
--- RetrieveDataFromFile/Program.cs
+++ RetrieveDataFromFile/Program.cs
@@ -6,8 +6,28 @@ namespace RetrieveDataFromFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***Retrieve Student Data from a Text File***");
+            Console.WriteLine("*****Retrieve Student Data from a Text File*****");
             FileProg.Do();
+            string input;
+            do
+            {
+                Console.WriteLine("*****Available Options*****");
+                Console.WriteLine("1.Show Sorted Data\n2.Search By Name");
+                int option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                {
+                    FileProg.SortByName();
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Input name ");
+                    FileProg.SearchByName(Console.ReadLine());
+                }
+                Console.WriteLine("Want to continue?(Type yes/no)");
+                input = Console.ReadLine();
+
+            } while (input.Equals("yes"));
 
         }
     }
